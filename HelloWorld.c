62ae6c72bbbac7ab0e373d01e90f6d665e1dc8b4
Park-SM <park97.sm@gmail.com>
2019-02-11 17:49:26 +0900

    Push at the SKTelecom

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
+#include <windows.h>
 
 int main(){
 	
-	printf("Hello, World!\n");
+	int i;
 	
+	printf("Hello, World!\n");
+	for (i = 1; i <= 10; i++)
+	    printf("%2d : Park Sang Min\n", i);
+	
+	system("PAUSE");
 	return 0;
 }
